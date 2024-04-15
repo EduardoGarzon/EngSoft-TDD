dcba7abfc85495db425a16f5a40fd9aa94668dd6
EduardoGarzon <luizeduardo3131@gmail.com>
2024-04-15 20:31:33 -0300

    terceiro commit

--- src/Dollar.java
+++ src/Dollar.java
@@ -2,17 +2,17 @@ class Dollar extends Money {
     Dollar(int amount) {
         this.amount = amount;
     }
-    int times(int multiplier) {
-        //return new Dollar(amount * multiplier);
-        return this.amount * multiplier;
+    Money times(int multiplier) {
+        return new Dollar(amount * multiplier);
+        //return this.amount * multiplier;
     }
 
-//    @Override
-//    public boolean equals(Object obj) {
-//        if (this == obj) return true;
-//        if (obj == null || getClass() != obj.getClass()) return false;
-//        Dollar other = (Dollar) obj;
-//        return amount == other.amount;
-//    }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Dollar other = (Dollar) obj;
+        return amount == other.amount;
+    }
 
 }
--- src/Franc.java
+++ src/Franc.java
@@ -2,8 +2,16 @@ class Franc extends Money{
     Franc(int amount) {
         this.amount= amount;
     }
-    int times(int multiplier) {
-        //return new Franc(amount * multiplier);
-        return this.amount * multiplier;
+    Money times(int multiplier) {
+        return new Franc(amount * multiplier);
+        //return this.amount * multiplier;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Franc other = (Franc) obj;
+        return amount == other.amount;
     }
 }
