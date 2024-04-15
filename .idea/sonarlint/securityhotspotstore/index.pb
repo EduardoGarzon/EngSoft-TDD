ef2ba117f340d235f5c3eeeaf36a8375bfbe852b
EduardoGarzon <luizeduardo3131@gmail.com>
2024-04-15 19:18:58 -0300

    first commit

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+?
+EngSoft-TDD.iml,e\4\e453f846abf9aec105c91296d97b4f9be166a974
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+?
+src/Dollar.java,d\0\d08d4036aa6cd74442f785637ba4b3d18f497256
+C
+src/DollarTest.java,2\4\249d9f6738ca687dc8bbcc05409b7bec393b9ba0
+C
+src/TestRunner.java,3\d\3da36a6b9c1fdd3875d2b74046ddd42946ad67ec
---
+++ src/TestRunner.java
@@ -0,0 +1,38 @@
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+    public static void main() {
+        Result result = JUnitCore.runClasses(DollarTest.class);
+        int count;
+        String msg;
+        long time = result.getRunTime();
+
+        System.out.println("\n\nRESULTADO DA EXECUÇÃO DOS TESTES");
+        System.out.println("Tempo: " + time + " ms");
+
+        if (result.wasSuccessful()) {
+            count = result.getRunCount();
+            if (count == 1)
+                msg = count + " teste executado com sucesso";
+            else
+                msg = count + " testes executados com sucesso";
+
+        }
+        else {
+            count = result.getFailureCount();
+            if (count == 1)
+                msg = count + " teste falhou";
+            else
+                msg = count + " testes falharam";
+        }
+
+        System.out.println(msg);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+    }
+}
